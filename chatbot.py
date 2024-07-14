6b3a298e444cf91aecbdbac53ee5254f7a00c97f
Lucas Azevedo <ls.lucasdesigner@gmail.com>
2024-07-14 12:16:25 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,13 @@
 # chatbot para atualização de demandas no final do dia, que serão encaminhadas para o dia seguinte [ produtora audiovisual ]
 
 #Clientes
-    # nome do cliente + data que entrou o pedido
+    # nome do cliente + nome do projeto + data que entrou o pedido [Castelinho - redes sociais + 15/07]
+    # observações da demanda: [fornecer informações quantidade de vídeos ou detalhes importante]
+
+
+#roteiro status [criação, aprovação, aprovado]
+#duração de roteiro [15", 30", 60"]
+
 #gravações [audio, video, off, drone]
     #formatos [full hd, ultra hd, 4k, 6k]
         #formatos de cor [slog, vídeo]
@@ -13,3 +19,4 @@
     #comercial para tv
         #quais emissoras 
         #qual duração de cada vt
+    # spot para ráido
