18dac9b427726359767b02b919fe5291a4a9ac0b
Lucas Azevedo <ls.lucasdesigner@gmail.com>
2024-07-14 11:58:33 -0400

    Create chatbot.py
    Descrição das primeiras atividades

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+# chatbot para atualização de demandas no final do dia, que serão encaminhadas para o dia seguinte [ produtora audiovisual ]
+
+#Clientes
+    # nome do cliente + data que entrou o pedido
+#gravações [audio, video, off, drone]
+    #formatos [full hd, ultra hd, 4k, 6k]
+        #formatos de cor [slog, vídeo]
+
+#projetos
+#finalizados
+#midias 
+    # videos para rede social
+    #comercial para tv
+        #quais emissoras 
+        #qual duração de cada vt
